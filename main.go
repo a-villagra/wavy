cebbb023a6f977e86fdcd271e4b889833fe5e4cf
Andres Villagra <andres.villagra@jampp.com>
2020-02-24 12:49:15 -0300

    Added sin wave distortion

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"math"
 
 	"github.com/go-audio/wav"
 )
@@ -42,12 +43,40 @@ func Run(stdout, stderr io.Writer) error {
 	defer f.Close()
 
 	d := wav.NewDecoder(f)
-	duration, err := d.Duration()
+	buf, err := d.FullPCMBuffer()
 	if err != nil {
+		errLog.Printf("Failed to get PCM buffer: %v", err)
 		return err
 	}
 
-	outLog.Printf("Original duration: %v", duration)
+	format := buf.Format
+	outLog.Printf("File info: Bit rate: %d, Format: %v", buf.SourceBitDepth, format)
+
+	nSamples := 0
+	for i := 0; i < len(buf.Data); i++ {
+		buf.Data[i] = int(float64(buf.Data[i]) * math.Cos(float64(nSamples) * 0.1))
+		nSamples++
+	}
+	outLog.Printf("Write %d samples", nSamples)
+
+	of, err := os.Create("out.wav")
+	if err != nil {
+		errLog.Printf("Failed to create output file: %v", err)
+		return err
+	}
+	defer of.Close()
+	e := wav.NewEncoder(
+		of,
+		format.SampleRate,
+		int(buf.SourceBitDepth),
+		format.NumChannels,
+		int(d.WavAudioFormat),
+	)
+	err = e.Write(buf)
+	if err != nil {
+		return err
+	}
+	e.Close()
 
 	return nil
 }
